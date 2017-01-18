540716f06dee41f714100843a16539a1cf8c7823
act262 <act262@gmail.com>
2017-01-18 18:04:43 +0800

    网络代理错误提示校验处理

--- app/src/main/java/io/micro/adt/ui/NetworkKitFragment.java
+++ app/src/main/java/io/micro/adt/ui/NetworkKitFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.text.Editable;
 import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,79 +20,136 @@ import io.micro.adt.util.text.SimpleTextWatcher;
 /**
  * 网络设置页
  */
-public class NetworkKitFragment extends BaseFragment {
+public class NetworkKitFragment extends BaseFragment implements CompoundButton.OnCheckedChangeListener {
+
+    private static final String PORT_NUMBER = "\\d{1,5}";
+    private Switch proxyButton;
+    private EditText hostText;
+    private EditText portText;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mRootView = inflater.inflate(R.layout.fragment_network_kit, container, false);
-        final Switch proxyButton = findView(R.id.proxySwitch);
-        final EditText hostText = findView(R.id.et_host);
-        final EditText portText = findView(R.id.et_port);
+        proxyButton = findView(R.id.proxySwitch);
+        hostText = findView(R.id.et_host);
+        portText = findView(R.id.et_port);
 
+        hostText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                String host = hostText.getText().toString().trim();
+                // 输入无效且聚焦状态
+                if (!validHost(host) && hasFocus) {
+                    hostErrorTip();
+                } else {
+                    hostText.setError(null);
+                }
+            }
+        });
+        portText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                String port = portText.getText().toString().trim();
+                if (!validPort(port) && hasFocus) {
+                    portErrorTip();
+                } else {
+                    portText.setError(null);
+                }
+            }
+        });
         hostText.addTextChangedListener(new SimpleTextWatcher() {
             @Override
             public void afterTextChanged(Editable s) {
                 String host = hostText.getText().toString().trim();
-                String port = portText.getText().toString().trim();
-
-            }
-        });
-
-        proxyButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                String host = hostText.getText().toString().trim();
-                String port = portText.getText().toString().trim();
-
-                // 开启状态必须指定HOST和port
-                if (isChecked) {
-                    if (TextUtils.isEmpty(host)) {
-                        hostText.setError("Host can't empty !");
-                        proxyButton.setChecked(false);
-                        preferences.edit().putBoolean(KEY_NETWORK_PROXY_CHECK, false).apply();
-                        return;
-                    }
-                    if (TextUtils.isEmpty(port)) {
-                        portText.setError("Port can't empty !");
-                        proxyButton.setChecked(false);
-                        preferences.edit().putBoolean(KEY_NETWORK_PROXY_CHECK, false).apply();
-                        return;
-                    }
-                    preferences.edit()
-                            .putString(KEY_NETWORK_PROXY_HOST, host)
-                            .putString(KEY_NETWORK_PROXY_PORT, port)
-                            .apply();
+                // 输入无效且聚焦状态
+                if (!validHost(host)) {
+                    hostErrorTip();
                 }
-
-                preferences.edit().putBoolean(KEY_NETWORK_PROXY_CHECK, isChecked).apply();
-                // TODO: 2017/1/16 校验IP,端口
-                NetworkKit.setProxy(getActivity(), isChecked, host, DFormatter.parseInt(port));
+            }
+        });
+        portText.addTextChangedListener(new SimpleTextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+                String port = portText.getText().toString().trim();
+                if (!validPort(port) && portText.hasFocus()) {
+                    portErrorTip();
+                }
             }
         });
 
-        String host = preferences.getString(KEY_NETWORK_PROXY_HOST, "");
-        String port = preferences.getString(KEY_NETWORK_PROXY_PORT, "8888");
+        proxyButton.setOnCheckedChangeListener(this);
+
+        return mRootView;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
 
         String proxyHost = NetworkKit.getProxyHost(getActivity());
         if (TextUtils.isEmpty(proxyHost)) {
-            hostText.setText(host);
-        } else {
-            hostText.setText(proxyHost);
+            proxyHost = preferences.getString(KEY_NETWORK_PROXY_HOST, "");
         }
+        hostText.setText(proxyHost);
+
         String proxyPort = NetworkKit.getProxyPort(getActivity());
         if (TextUtils.isEmpty(proxyPort)) {
-            portText.setText(port);
-        } else {
-            portText.setText(proxyPort);
+            proxyPort = preferences.getString(KEY_NETWORK_PROXY_PORT, "8888");
         }
+        portText.setText(proxyPort);
 
         boolean checked = preferences.getBoolean(KEY_NETWORK_PROXY_CHECK, false);
         proxyButton.setChecked(checked);
-        return mRootView;
+    }
+
+    private boolean validHost(String host) {
+        return Patterns.IP_ADDRESS.matcher(host).matches();
+    }
+
+    private boolean validPort(String port) {
+        return port.matches(PORT_NUMBER);
+    }
+
+    private void hostErrorTip() {
+        hostText.setError("Host invalid");
+    }
+
+    private void portErrorTip() {
+        portText.setError("Port invalid");
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        String host = hostText.getText().toString().trim();
+        String port = portText.getText().toString().trim();
+
+        // 开启状态必须指定HOST和port
+        if (isChecked) {
+            if (TextUtils.isEmpty(host) || !validHost(host)) {
+                hostErrorTip();
+                proxyButton.setChecked(false);
+                preferences.edit().putBoolean(KEY_NETWORK_PROXY_CHECK, false).apply();
+                return;
+            }
+            if (TextUtils.isEmpty(port) || !validPort(port)) {
+                portErrorTip();
+                proxyButton.setChecked(false);
+                preferences.edit().putBoolean(KEY_NETWORK_PROXY_CHECK, false).apply();
+                return;
+            }
+            preferences.edit()
+                    .putString(KEY_NETWORK_PROXY_HOST, host)
+                    .putString(KEY_NETWORK_PROXY_PORT, port)
+                    .apply();
+        }
+
+        preferences.edit().putBoolean(KEY_NETWORK_PROXY_CHECK, isChecked).apply();
+        NetworkKit.setProxy(getActivity(), isChecked, host, DFormatter.parseInt(port));
     }
 
     public static final String KEY_NETWORK_PROXY_HOST = "proxy_host";
     public static final String KEY_NETWORK_PROXY_PORT = "proxy_port";
     public static final String KEY_NETWORK_PROXY_CHECK = "proxy_check";
+
 }
